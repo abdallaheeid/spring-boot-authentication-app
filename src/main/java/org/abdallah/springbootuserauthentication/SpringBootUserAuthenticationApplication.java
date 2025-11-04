dd5b5344480fb73d9bba190936acf0d2e0c010b6
Abdallah Eid <abdallaheeid@icloud.com>
2025-11-04 21:02:09 +0100

    git init

---
+++ src/main/java/org/abdallah/springbootuserauthentication/SpringBootUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package org.abdallah.springbootuserauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUserAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/abdallah/springbootuserauthentication/SpringBootUserAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abdallah.springbootuserauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
